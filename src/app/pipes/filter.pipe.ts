4a73f7a50122e3741439e032bafad4c6e91d5aa8
Monika <monika.radosiewicz@gmail.com>
2019-02-26 10:31:28 +0100

    fix filtering - it display empty book list on app start becuse of null filter string

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -6,7 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, filterString: string, propName: string): any {
-    if(value === null || value.length === 0) {
+  
+    if(value === null || value.length === 0 || !filterString) {
       return value
     }
     return value.filter(el => el[propName].toLowerCase().includes(filterString))
